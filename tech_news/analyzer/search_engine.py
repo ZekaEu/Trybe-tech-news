e01bc2b98e6f497d54565bdef086a7f8cd6b00b5
ZekaEu <zeka.surf@hotmail.com>
2022-07-25 03:19:10 -0300

    Requisitos 6, 8 e 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,11 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    return [(noticia["title"], noticia["url"]) for noticia in search_news({
+        "title": {"$regex": title, "$options": "i"}
+    })]
 
 
 # Requisito 7
@@ -10,9 +15,13 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    return [(noticia["title"], noticia["url"]) for noticia in search_news({
+        "tags": {"$regex": tag, "$options": "i"}
+    })]
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    return [(noticia["title"], noticia["url"]) for noticia in search_news({
+        "category": {"$regex": category, "$options": "i"}
+    })]
